ea4763b3290da48a0297fad285842e99980c1d2c
USER <USER@DESKTOP-PKPLFQG>
2024-10-25 15:18:47 +0700

    Add project files.

---
+++ MauiCarRace/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace MauiCarRace
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new AppShell();
+            MainPage = new MainPage();
+
+        }
+    }
+}
